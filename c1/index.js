f9c199207cce8b5b01c485404db50fade9ae9786
Dragan Tomovski <32159088+dragantomas@users.noreply.github.com>
2017-11-16 13:29:35 +0100

    Prv Cas Java Skript

---
+++ c1/index.js
@@ -0,0 +1,22 @@
+console.log('Hello World!');
+
+var ime = "Bojan"; // string (низа од карактери)
+
+var godini = 35; // number (низа од бројки)
+
+var visina = 182.5; // number
+
+var temperatura = -22.5; // number
+
+var classEnded = true; // boolean 
+
+console.log(ime);
+console.log(godini);
+console.log(visina);
+console.log(temperatura);
+console.log(classEnded);
+
+
+godinaNaRagjanje = 1983 // camel case style
+
+godina_na_ragjanje = 1983 // snake case style
