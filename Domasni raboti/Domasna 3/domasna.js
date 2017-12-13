19744e6e9a32a2f761e9d42a223b58b47b368300
dragantomas <32159088+dragantomas@users.noreply.github.com>
2017-12-13 19:10:03 +0100

    Domasna Sreda 131217

--- Domasni raboti/Domasna 3/domasna.js
+++ Domasni raboti/Domasna 3/domasna.js
@@ -1,36 +1,13 @@
-// document.getElementById("googlelink").addEventListener("click", redirectToYahoo);
-// document.getElementById("googlelink").addEventListener("auxclick", redirectToYahooNewTab);
-// document.getElementById("googlelink").addEventListener("contextmenu", rightClick);
+document.getElementById("googlelink").addEventListener("click", redirectToYahoo);
+document.getElementById("googlelink").addEventListener("auxclick", redirectToYahooNewTab);
 
+function redirectToYahoo() {
+ 	this.href = "https://www.yahoo.com";
+}
 
-// function redirectToYahoo() {
-//  	window.location.href = "https://www.yahoo.com";
-// }
-
-// function redirectToYahooNewTab() {
-//  	window.open('https://www.yahoo.com', '_blank');
-// }
-
-// function rightClick() {
-// 	event.preventDefault();
-// };
-
-var link = document.getElementById('googlelink');
-
-link.addEventListener('click', function(e){
- window.location.href = "https://www.yahoo.com";
-});
-
-link.addEventListener("auxclick",function(e){
-   if(e.button !==2){
-      window.open('https://www.yahoo.com', '_blank');
-   }else{
-     e.preventDefault()
-   } 
-});
-
-link.addEventListener('contextmenu', function(e){
-});
+function redirectToYahooNewTab() {
+	this.href = "https://www.yahoo.com", "_blank";
+}
 
 function presmetaj(){
 	var prvBroj = document.LiveCalculator.Broj1.value;
